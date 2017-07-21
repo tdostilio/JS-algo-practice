0b93523ca2ed098d31a5e5308e701e142237bd1b
tdostilio <tdostilio@gmail.com>
2017-07-21 11:27:46 -0400

    finished algo code challenges

--- algo1.js
+++ algo1.js
@@ -38,3 +38,18 @@ fibonacci(4000000);
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
 // // Answer: 6857
+
+
+function findFactors(number) {
+    for (var i=2; i<=number;) {
+        if (number % i ==0) {
+            number /= i;
+        }
+        else {
+            i++;
+        }
+    }
+    console.log(i);
+}
+
+findFactors(600851475143);
